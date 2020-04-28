17562e52f19fe1e27eb5db03774b6fb65fdc7fbf
Rasjaad Basarat <rasjaad10@msn.com>
2020-04-28 16:39:01 +0200

    Added merchant vouchers model

---
+++ FindeyVouchers/FindeyVouchers.Domain/EfModels/MerchantVoucher.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FindeyVouchers.Domain.EfModels
+{
+    public class MerchantVoucher
+    {
+        public Guid Id { get; set; }
+        public ApplicationUser Merchant { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime ValidUntil { get; set; }
+        public Decimal Price { get; set; }
+    }
+}

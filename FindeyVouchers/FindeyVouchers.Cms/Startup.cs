c20fa25252b3c34cd12516985055fda904c25951
Rasjaad Basarat <rasjaad10@msn.com>
2020-04-28 11:35:47 +0200

    Added password settings

--- FindeyVouchers/FindeyVouchers.Cms/Startup.cs
+++ FindeyVouchers/FindeyVouchers.Cms/Startup.cs
@@ -38,6 +38,14 @@ namespace FindeyVouchers.Cms
             // Configure identity
             services.Configure<IdentityOptions>(options =>
             {
+                // Password settings.
+                options.Password.RequireDigit = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequiredLength = 6;
+                options.Password.RequiredUniqueChars = 1;
+                
                 // User settings.
                 options.User.RequireUniqueEmail = false;
             });

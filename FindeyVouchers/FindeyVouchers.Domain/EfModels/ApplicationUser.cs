1afc3a2c14eabeb40eab0cdf37598e2f98acf483
Rasjaad Basarat <rasjaad10@msn.com>
2020-04-28 20:34:00 +0200

    Added fields to user

--- FindeyVouchers/FindeyVouchers.Domain/EfModels/ApplicationUser.cs
+++ FindeyVouchers/FindeyVouchers.Domain/EfModels/ApplicationUser.cs
@@ -4,6 +4,19 @@ namespace FindeyVouchers.Domain.EfModels
 {
     public class ApplicationUser : IdentityUser
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Street { get; set; }
+        public string ZipCode { get; set; }
         public string CompanyName { get; set; }
+        public int KvkNumber { get; set; }
+        public string IbanNumber { get; set; }
+        public LegalEntity LegalEntity { get; set; }
+    }
+
+    public enum LegalEntity
+    {
+        Individual,
+        Business
     }
 }

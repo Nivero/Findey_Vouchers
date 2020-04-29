c584ad9db9db2085792007fe53fe27f9010e6fd8
Rasjaad Basarat <rasjaad10@msn.com>
2020-04-29 17:24:10 +0200

    Updated merchant model

--- FindeyVouchers/FindeyVouchers.Domain/EfModels/ApplicationUser.cs
+++ FindeyVouchers/FindeyVouchers.Domain/EfModels/ApplicationUser.cs
@@ -7,14 +7,23 @@ namespace FindeyVouchers.Domain.EfModels
     public class ApplicationUser : IdentityUser
     {
         public string StripeAccountId { get; set; }
-        public string Country { get; set; }
-        public BusinessType BusinessType { get; set; }
+        
+        [Display(Name = "Voornaam")]
         public string FirstName { get; set; }
+        [Display(Name = "Achternaam")]
         public string LastName { get; set; }
+        [Display(Name = "Geboortedatum")]
         [DataType(DataType.Date)] public DateTime DateOfBirth { get; set; }
+        [Display(Name = "Address")]
         public string Address { get; set; }
-        public string City { get; set; }
+        [Display(Name = "Postcode")]
         public string ZipCode { get; set; }
+        [Display(Name = "Stad")]
+        public string City { get; set; }
+        [Display(Name = "Land")]
+        public string Country { get; set; }
+        [Display(Name = "Rechtsvorm bedrijf")]
+        public BusinessType BusinessType { get; set; }
         
     }
 

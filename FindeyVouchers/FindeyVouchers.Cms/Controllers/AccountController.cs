f84ed7c0e9a8f01203b1eb2c71882c7defe7a6b6
Rasjaad Basarat <rasjaad10@msn.com>
2020-04-29 19:22:47 +0200

    Updated merchant accound wip onboarding

---
+++ FindeyVouchers/FindeyVouchers.Cms/Controllers/AccountController.cs
@@ -0,0 +1,96 @@
+using System.Linq;
+using System.Threading.Tasks;
+using FindeyVouchers.Domain;
+using FindeyVouchers.Domain.EfModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FindeyVouchers.Cms.Controllers_
+{
+    public class AccountController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public AccountController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        // GET: Account/Edit/5
+        public async Task<IActionResult> Index()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            var applicationUser = await _context.Users.FindAsync(user.Id);
+            if (applicationUser == null)
+            {
+                return NotFound();
+            }
+
+            return View(applicationUser);
+        }
+
+        // POST: Account/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Index(string id,
+            [Bind(
+                "StripeAccountId,FirstName,LastName,DateOfBirth,Address,ZipCode,City,Country,BusinessType,PhoneNumber,Id,UserName,NormalizedUserName,Email,NormalizedEmail,EmailConfirmed,PasswordHash,SecurityStamp,ConcurrencyStamp,PhoneNumberConfirmed,TwoFactorEnabled,LockoutEnd,LockoutEnabled,AccessFailedCount")]
+            ApplicationUser applicationUser)
+        {
+            if (id != applicationUser.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var user = await _userManager.GetUserAsync(User);
+                    user.FirstName = applicationUser.FirstName;
+                    user.LastName = applicationUser.LastName;
+                    user.DateOfBirth = applicationUser.DateOfBirth;
+                    user.Address = applicationUser.Address;
+                    user.ZipCode = applicationUser.ZipCode;
+                    user.City = applicationUser.City;
+                    user.BusinessType = applicationUser.BusinessType;
+                    user.PhoneNumber = applicationUser.PhoneNumber;
+                    user.Email = applicationUser.Email;
+                    user.NormalizedEmail = applicationUser.Email.ToUpper();
+
+                    await _userManager.UpdateAsync(user);
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ApplicationUserExists(applicationUser.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(applicationUser);
+        }
+
+        private bool ApplicationUserExists(string id)
+        {
+            return _context.Users.Any(e => e.Id == id);
+        }
+    }
+}
--- FindeyVouchers/FindeyVouchers.Domain/EfModels/ApplicationUser.cs
+++ FindeyVouchers/FindeyVouchers.Domain/EfModels/ApplicationUser.cs
@@ -7,32 +7,29 @@ namespace FindeyVouchers.Domain.EfModels
     public class ApplicationUser : IdentityUser
     {
         public string StripeAccountId { get; set; }
-        
-        [Display(Name = "Voornaam")]
-        public string FirstName { get; set; }
-        [Display(Name = "Achternaam")]
-        public string LastName { get; set; }
+
+        [Display(Name = "Voornaam")] public string FirstName { get; set; }
+        [Display(Name = "Achternaam")] public string LastName { get; set; }
+
         [Display(Name = "Geboortedatum")]
-        [DataType(DataType.Date)] public DateTime DateOfBirth { get; set; }
-        [Display(Name = "Address")]
-        public string Address { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime DateOfBirth { get; set; }
+
+        [Display(Name = "Adres")] public string Address { get; set; }
+
         [Display(Name = "Postcode")]
+        [RegularExpression(@"^\d{4} ?[aA-zZ]{2}$", ErrorMessage = "Ongeldige postcode")]
         public string ZipCode { get; set; }
-        [Display(Name = "Stad")]
-        public string City { get; set; }
-        [Display(Name = "Land")]
-        public string Country { get; set; }
-        [Display(Name = "Rechtsvorm bedrijf")]
-        public BusinessType BusinessType { get; set; }
-        
+
+        [Display(Name = "Stad")] public string City { get; set; }
+        [Display(Name = "Land")] public string Country { get; set; }
+        [Display(Name = "Rechtsvorm bedrijf")] public BusinessType BusinessType { get; set; }
+        [DataType(DataType.PhoneNumber)] public override string PhoneNumber { get; set; }
     }
 
     public enum BusinessType
     {
-        SoleProp,
-        Corporation,
-        NonProfit,
-        Partnership,
-        Llc
+        [Display(Name = "Eenmanszaak")] SoleProp,
+        [Display(Name = "Bedrijf")] Corporation
     }
 }

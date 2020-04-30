89759532d118feb6705b763d4100593d8ad3bf6c
Rasjaad Basarat <rasjaad10@msn.com>
2020-04-30 16:24:45 +0200

    Added functionality for searching vouchers

--- FindeyVouchers/FindeyVouchers.Cms/Controllers/CustomerVoucherController.cs
+++ FindeyVouchers/FindeyVouchers.Cms/Controllers/CustomerVoucherController.cs
@@ -2,26 +2,43 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FindeyVouchers.Cms.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using FindeyVouchers.Domain;
 using FindeyVouchers.Domain.EfModels;
+using Microsoft.AspNetCore.Identity;
 
 namespace FindeyVouchers.Cms.Controllers_
 {
     public class CustomerVoucherController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public CustomerVoucherController(ApplicationDbContext context)
+        public CustomerVoucherController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
         {
+            _userManager = userManager;
             _context = context;
         }
 
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string query)
         {
-            return View(await _context.CustomerVouchers.ToListAsync());
+            var user = await _userManager.GetUserAsync(User);
+            var customerVouchers = _context.CustomerVouchers.Where(x => x.VoucherMerchant.Merchant == user);
+
+            if (!string.IsNullOrEmpty(query))
+            {
+                customerVouchers = customerVouchers.Where(s => s.Code.Contains(query));
+            }
+
+            var model = new CustomerVoucherViewModel
+            {
+                Vouchers = customerVouchers
+            };
+
+            return View(model);
         }
 
         public async Task<IActionResult> Details(Guid? id)
@@ -33,7 +50,9 @@ namespace FindeyVouchers.Cms.Controllers_
 
             var customerVoucher = await _context.CustomerVouchers
                 .FirstOrDefaultAsync(m => m.Id == id);
-            if (customerVoucher == null)
+            var user = await _userManager.GetUserAsync(User);
+
+            if (customerVoucher.VoucherMerchant.Merchant != user)
             {
                 return NotFound();
             }

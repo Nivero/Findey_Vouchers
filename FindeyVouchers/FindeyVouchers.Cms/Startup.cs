bae3b03ba4649d50d51ddf78f0c2c06581eae061
Rasjaad Basarat <rasjaad10@msn.com>
2020-04-30 15:13:52 +0200

    minor changes

--- FindeyVouchers/FindeyVouchers.Cms/Startup.cs
+++ FindeyVouchers/FindeyVouchers.Cms/Startup.cs
@@ -16,6 +16,7 @@ namespace FindeyVouchers.Cms
 {
     public class Startup
     {
+        
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -26,11 +27,14 @@ namespace FindeyVouchers.Cms
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            
+
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
-            
-            services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+                
+            });
+
+             services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
@@ -75,6 +79,7 @@ namespace FindeyVouchers.Cms
             services.AddTransient<IAzureStorageService, AzureStorageService>();
             // services.AddTransient<IEmailSender, IdentityCoreEmailSender>();
             // services.Configure<Domain.SendGrid>(Configuration);
+            services.BuildServiceProvider().GetService<ApplicationDbContext>().Database.Migrate();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

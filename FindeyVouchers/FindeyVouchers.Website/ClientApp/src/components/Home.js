947d369cab67a767ea8255f3a22e9ee47da59ecf
Rasjaad Basarat <rasjaad10@msn.com>
2020-05-02 17:56:06 +0200

    added logic for header data

--- FindeyVouchers/FindeyVouchers.Website/ClientApp/src/components/Home.js
+++ FindeyVouchers/FindeyVouchers.Website/ClientApp/src/components/Home.js
@@ -6,17 +6,30 @@ import CheckoutBar from "./CheckoutBar/CheckoutBar";
 export class Home extends Component {
     constructor(props) {
         super(props);
+        this.state = {response: {}};
     }
 
     componentDidMount() {
+        this.fetchMerchant();
     }
 
+    async fetchMerchant() {
+        const requestOptions = {
+            method: 'GET',
+        };
+        fetch(`merchant/info/nivero`, requestOptions)
+            .then((response) => response.json())
+            .then((response) => {
+                this.setState({
+                    response: response
+                });
+            });
+    }
     render() {
-
-
+        
         return (
             <div className="d-flex flex-column justify-content-start">
-                <VoucherPageHeader/>
+                <VoucherPageHeader merchant={this.state.response}/>
                 <div className="d-flex flex-row justify-content-center">
                     <Voucher/>
                 </div>
--- FindeyVouchers/FindeyVouchers.Website/ClientApp/src/components/voucherPageHeader/VoucherPageHeader.js
+++ FindeyVouchers/FindeyVouchers.Website/ClientApp/src/components/voucherPageHeader/VoucherPageHeader.js
@@ -1,6 +1,7 @@
 ﻿import React, {Component} from 'react';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faMapMarkerAlt} from "@fortawesome/free-solid-svg-icons";
+
 export default class VoucherPageHeader extends Component {
     constructor(props) {
         super(props);
@@ -8,28 +9,33 @@ export default class VoucherPageHeader extends Component {
     }
 
     componentDidMount() {
+
     }
 
+
     render() {
+        console.log(this.props.merchant)
         return (
             <div className="d-flex justify-content-start background-color-default text-white">
                 <div className="mr-5 w-75 p-2 text-center">Company name</div>
                 <div className="d-flex justify-content-center flex-column border-left p-2 mt-2 mb-2">
                     <div className="d-block">
                         <FontAwesomeIcon icon={faMapMarkerAlt} className="mr-2"/>
-                        <span>Locatie</span>
+                        <span>{this.props.merchant.name}</span>
                     </div>
                     <div className="d-block">
                         <FontAwesomeIcon icon={faMapMarkerAlt} className="mr-2"/>
-                        <span>Website</span>
+                        <a href={this.props.merchant.website} className="text-white">
+                            <span>{this.props.merchant.website}</span>
+                        </a>
                     </div>
                     <div className="d-block">
                         <FontAwesomeIcon icon={faMapMarkerAlt} className="mr-2"/>
-                        <span>E-mail</span>
+                        <span>{this.props.merchant.email}</span>
                     </div>
                     <div className="d-block">
                         <FontAwesomeIcon icon={faMapMarkerAlt} className="mr-2"/>
-                        <span>Telefoon</span>
+                        <span>{this.props.merchant.phoneNumber}</span>
                     </div>
                 </div>
             </div>

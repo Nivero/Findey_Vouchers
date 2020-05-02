7b0b7ac350ec72c38a7ab533ec08d110c2d27b28
Rasjaad Basarat <rasjaad10@msn.com>
2020-05-02 12:41:12 +0200

    Cleaned up front end

--- FindeyVouchers/FindeyVouchers.Website/ClientApp/src/components/navMenu/NavMenu.js
+++ FindeyVouchers/FindeyVouchers.Website/ClientApp/src/components/navMenu/NavMenu.js
@@ -26,18 +26,13 @@ export class NavMenu extends Component {
       <header>
         <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" light>
           <Container>
-            <NavbarBrand tag={Link} to="/">FindeyVouchers.Website</NavbarBrand>
+            <NavbarBrand tag={Link} to="/">Findey Vouchers</NavbarBrand>
             <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
-            <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
+            <Collapse className="d-sm-inline-flex flex-sm-row-reverse justify-content-center" isOpen={!this.state.collapsed} navbar>
               <ul className="navbar-nav flex-grow">
                 <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/counter">Counter</NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
+                  <h4>Overzicht vouchers</h4>
+                  {/*<NavLink tag={Link} className="text-dark" to="/">Home</NavLink>*/}
                 </NavItem>
               </ul>
             </Collapse>

466b39561e4207b47344d37314daa07d99c51d78
Rasjaad Basarat <rasjaad10@msn.com>
2020-05-04 14:19:43 +0200

    Update mail sender

--- FindeyVouchers/FindeyVouchers.Cms/Startup.cs
+++ FindeyVouchers/FindeyVouchers.Cms/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -77,8 +78,9 @@ namespace FindeyVouchers.Cms
             
             services.AddTransient<IVoucherService, VoucherService>();
             services.AddTransient<IAzureStorageService, AzureStorageService>();
-            // services.AddTransient<IEmailSender, IdentityCoreEmailSender>();
-            // services.Configure<Domain.SendGrid>(Configuration);
+            services.AddTransient<IEmailSender, IdentityCoreEmailSender>();
+            services.AddTransient<IMailService, MailService>();
+            services.Configure<Domain.SendGrid>(Configuration);
             services.BuildServiceProvider().GetService<ApplicationDbContext>().Database.Migrate();
         }
 
---
+++ FindeyVouchers/FindeyVouchers.Services/MailService.cs
@@ -0,0 +1,43 @@
+﻿using System.Threading.Tasks;
+using FindeyVouchers.Domain.EfModels;
+using FindeyVouchers.Interfaces;
+using Microsoft.Extensions.Configuration;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+
+namespace FindeyVouchers.Services
+{
+    public class MailService : IMailService
+    {
+        private readonly string _apiKey;
+        private readonly string _senderEmail;
+        private readonly string _senderName;
+
+        public MailService(IConfiguration configuration)
+        {
+            var config = configuration;
+            _apiKey = config.GetValue<string>("SendGridKey");
+            _senderEmail = config.GetValue<string>("EmailSenderAddress");
+            _senderName = config.GetValue<string>("EmailSenderName");
+        }
+
+        public Task<Response> SendVoucherMail(Customer customer, ApplicationUser user)
+        {
+            var subject = $"Je voucher van {user.CompanyName}";
+
+            return SendMail(customer.Email, subject, "test");
+        }
+
+
+        private async Task<Response> SendMail(string receiverAddress, string subject, string body)
+        {
+            var client = new SendGridClient(_apiKey);
+            var from = new EmailAddress(_senderEmail, _senderName);
+            var to = new EmailAddress(receiverAddress);
+
+
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, body, body);
+            return await client.SendEmailAsync(msg);
+        }
+    }
+}

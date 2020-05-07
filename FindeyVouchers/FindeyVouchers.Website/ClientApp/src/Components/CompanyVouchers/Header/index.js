b6d0e9f47bf9765c82422033fca207734911f4d7
Rasjaad Basarat <rasjaad10@msn.com>
2020-05-07 18:04:14 +0200

    Fixed voucherimages

--- FindeyVouchers/FindeyVouchers.Website/ClientApp/src/Components/CompanyVouchers/Header/index.js
+++ FindeyVouchers/FindeyVouchers.Website/ClientApp/src/Components/CompanyVouchers/Header/index.js
@@ -5,7 +5,6 @@ import './header.css';
 
 export default class Header extends React.Component {
     render() {
-        console.log(this.props.merchant)
         return (
             <header className="mci-header-container">
                 <Row>

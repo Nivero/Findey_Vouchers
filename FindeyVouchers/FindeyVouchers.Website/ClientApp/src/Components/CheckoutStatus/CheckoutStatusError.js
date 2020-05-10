fe015fefbb2fbf4bdcd8c651d7ccb63f526308f5
Rasjaad Basarat <rasjaad10@msn.com>
2020-05-10 12:28:26 +0200

    Changed icon on error page

--- FindeyVouchers/FindeyVouchers.Website/ClientApp/src/Components/CheckoutStatus/CheckoutStatusError.js
+++ FindeyVouchers/FindeyVouchers.Website/ClientApp/src/Components/CheckoutStatus/CheckoutStatusError.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Container, Jumbotron, Button } from 'reactstrap';
-import { FaCreativeCommonsNcEu} from "react-icons/fa";
+import { FaExclamationCircle} from "react-icons/fa";
 import { Link } from 'react-router-dom';
 import './checkoutStatus.css';
 
@@ -9,12 +9,12 @@ export default class CheckoutStatusError extends React.Component {
         return (
             <Container>
                     <Jumbotron>
-                        <h1 className="display-5 text-uppercase text-danger"><FaCreativeCommonsNcEu size={64} /> Something Went Wrong</h1>
+                        <h1 className="display-5 text-uppercase text-danger"><FaExclamationCircle size={64} /> Something Went Wrong</h1>
                         <hr className="my-2" />
                         <p>Something went wrong. Try one more again.</p>
                         <p className="lead">
-                        <Link to="/checkout"><Button color="danger">Try Again</Button></Link>{' '}
-                        <Link to="/"><Button color="secondary">Go to Voucherslist</Button></Link>
+                        {/*<Link to="/checkout"><Button color="danger">Try Again</Button></Link>*/}
+                        <Link to="/"><Button color="secondary">Go to Homepage</Button></Link>
                         </p>
                     </Jumbotron>
             </Container>

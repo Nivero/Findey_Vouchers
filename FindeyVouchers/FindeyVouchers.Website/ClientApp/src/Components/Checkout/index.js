ef39874d2c3ce435420a55eb4986b68c8671645f
Stefan Solopa <stefan.solopa@lifeworks.com>
2020-05-21 10:33:56 +0300

    added stripe checkout page

--- FindeyVouchers/FindeyVouchers.Website/ClientApp/src/Components/Checkout/index.js
+++ FindeyVouchers/FindeyVouchers.Website/ClientApp/src/Components/Checkout/index.js
@@ -1,42 +1,48 @@
 import React from 'react';
-import {Container, Row} from 'reactstrap';
-import {connect} from 'react-redux';
+import { Container, Row } from 'reactstrap';
+import { connect } from 'react-redux';
 import './checkout.css';
-import {loadStripe} from '@stripe/stripe-js';
-import OrderOverView from "./OrderOverView";
-import {InjectedCheckoutForm} from "./CheckoutForm";
-import {Elements} from "@stripe/react-stripe-js";
+import { loadStripe } from '@stripe/stripe-js';
+import OrderOverView from './OrderOverView';
+import { InjectedCheckoutForm } from './CheckoutForm';
+import { Elements } from '@stripe/react-stripe-js';
+import TopHeader from '../../common/components/top-header/TopHeader';
+
 
 const stripePromise = loadStripe('pk_live_EnmsulJpsuqfx7wpKTqWDlON00nbiPNe1s');
 const
-    Checkout = (props) => {
-        return (
-            <Container className="p-3 bg-secondary my-2 rounded">
-                <span className="text-center text-light"><h1>Uw Bestelling</h1></span>
-                <Row>
-                    <OrderOverView cartItems={props.cartItems} cartTotal={props.cartTotal}/>
-                    <Elements stripe={stripePromise}>
-                        <InjectedCheckoutForm totalAmount={props.cartTotal} cartItems={props.cartItems}/>
-                    </Elements>
+  Checkout = (props) => {
+    return (
+      <div>
+        <TopHeader title={'JOUW ORDER'}/>
 
-                </Row>
-                <hr/>
-                <span className="text-center text-light">
-        <p>Bij het bestellen van de voucher(s), gaat u akkoord met onze <a href="">Voorwaarden & Privacy</a></p>
-      </span>
-            </Container>
-        );
-    };
+        <Container className="mt-4">
+
+          <Elements stripe={stripePromise}>
+            <InjectedCheckoutForm totalAmount={props.cartTotal} cartItems={props.cartItems}>
+              <OrderOverView cartItems={props.cartItems} cartTotal={props.cartTotal}/>
+            </InjectedCheckoutForm>
+          </Elements>
+
+          <hr/>
+
+          <div className="text-center text-secondary">
+            <p>Bij het bestellen van de voucher(s), gaat u akkoord met onze <a href="">Voorwaarden & Privacy</a></p>
+          </div>
+        </Container>
+      </div>
+    );
+  };
 
 
 const
-    mapStateToProps = state => {
-        return {cartTotal: state.cartTotal, cartItems: state.cartItems}
-    }
+  mapStateToProps = state => {
+    return { cartTotal: state.cartTotal, cartItems: state.cartItems }
+  }
 
 export default connect(mapStateToProps)
 
 (
-    Checkout
+  Checkout
 )
 ;
